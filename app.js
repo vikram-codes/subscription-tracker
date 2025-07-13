7d95a52db93776d8731a44e71e7d64a1b1649eac
vikram-codes <vikramjit.personal@gmail.com>
2025-07-13 02:14:14 -0400

    Configuring the files

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 
+import { PORT } from "./config/env.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Subscription Tracker");
 });
 
-app.listen(3000, () => {
-  console.log("Subsription Tracker running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Subsription Tracker running on http://localhost:${PORT}`);
 });
 
 export default app;
