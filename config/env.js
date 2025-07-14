95ea2641f8731b90813208ed5b421f7be6cec070
vikram-codes <vikramjit.personal@gmail.com>
2025-07-14 05:42:46 -0400

    Adding Reminder workflow

--- config/env.js
+++ config/env.js
@@ -6,9 +6,12 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 export const {
   PORT,
   NODE_ENV,
+  SERVER_URL,
   DB_URI,
   JWT_SECRET,
   JWT_EXPIRES_IN,
   ARCJET_KEY,
   ARCJET_ENV,
+  QSTASH_TOKEN,
+  QSTASH_URL,
 } = process.env;
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -6,9 +8,23 @@ export const createSubscription = async (req, res, next) => {
       ...req.body,
       user: req.user._id,
     });
-    res.status(201).json({ success: true, data: subscription });
-  } catch (error) {
-    next(error);
+
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        "content-type": "application/json",
+      },
+      retries: 0,
+    });
+
+    res
+      .status(201)
+      .json({ success: true, data: { subscription, workflowRunId } });
+  } catch (e) {
+    next(e);
   }
 };
 
