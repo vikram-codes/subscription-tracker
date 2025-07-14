23246cd6306f15d4b46b2b431f87cdce46d5f6af
vikram-codes <vikramjit.personal@gmail.com>
2025-07-14 03:54:19 -0400

    Implementing Arcjet Middleware Security

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ import userRouter from "./routes/user.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
